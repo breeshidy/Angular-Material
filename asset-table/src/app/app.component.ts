44506982c741fe125dfa6864bac21b79197370e7
Bridget Shidok <bridget.shidok@microlise.com>
2022-10-19 15:21:30 +0100

    sorting abilities

--- asset-table/src/app/app.component.ts
+++ asset-table/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+
+export interface Assets {
+  id: number;
+  name: string;
+  status: number;
+  lastUpdatedDate: Date;
+}
+
+const ASSET_DATA: Assets[] = [
+  { id: 1, name: 'Gate A', status: 1.0079, lastUpdatedDate: new Date('01/16/2021 1:30:59') },
+  { id: 2, name: 'Barrier A', status: 2.0079, lastUpdatedDate: new Date('01/17/2021 1:10:39') },
+  { id: 3, name: 'Gate B', status: 3.0079, lastUpdatedDate: new Date('01/18/2021 1:30:59') },
+  { id: 4, name: 'Barrier B', status: 4.0079, lastUpdatedDate: new Date('01/19/2021 1:20:49') },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'asset-table';
+  title = 'Asset Table';
+  displayedColumns: string[] = ['id', 'name', 'status', 'lastUpdatedDate'];
+  dataSource = new MatTableDataSource(ASSET_DATA);
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
+
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
--- asset-table/src/app/app.module.ts
+++ asset-table/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';  
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatTableModule,
+    MatSortModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
